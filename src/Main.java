6daa793ace1cdce352de3ea4f1e9f2afbef64ac4
RasmusSamuelsson <rasmus.samuelsson@gmail.com>
2022-11-09 10:23:11 +0100

    Test 2 rad 3

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 
         System.out.println("Test 2 hemma!");
         System.out.println("Rad 2 test 2");
+        System.out.println("Rad 3 test 2, test utan att pullat nya merge från clone");
     }
 }
